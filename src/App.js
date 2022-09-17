cd5e6a0a2f95fbe1b0193854dd69a52b4a12c027
bipularyal <59837845+bipularyal@users.noreply.github.com>
2022-09-16 23:10:35 -0500

    completed building searchbox and title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"
-import MovieList from './components/movielist';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+
+
 function App() {
   const [movies, setMovies] = useState([]);
-  const getMovieRequest = () => {
+  const [searchValue,setSearchValue] = useState("");
+  const getMovieRequest = async() => {
     const url = "https://www.omdbapi.com/?s=harry&apikey=fbabb382"
-  }
+    const response = await fetch(url);
+    const resJson = await response.json();
+    setMovies(resJson.Search)
+  };
+  useEffect(()=> {
+    getMovieRequest();
+  },[]);
   return (
     <div className='container-fluid movie-app'>
+      <div className="row">
+        <MovieListHeading heading = "Movies"/>
+      </div>
       <div className='row'>
       <MovieList movies = {movies}/>
+      <SearchBox/>
       </div>
 
     </div>
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SearchBox = (props) => {
+    return(
+    <div className="col com-sm-4">
+        <input type="text" className="form-control" placeholder='Type to search...'/>
+    </div>)
+    
+}
+export default SearchBox
